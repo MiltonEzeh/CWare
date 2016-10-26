96d3a92c567254da1abff9664cd91711e3a1e787
Milton Ezeh <chikere@gmail.com>
2016-10-26 17:29:50 +0100

    CWare
    Courseware app for indy languages

---
+++ src/main/java/com/i3create/cware/CWareApplication.java
@@ -0,0 +1,12 @@
+package com.i3create.cware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CWareApplication.class, args);
+	}
+}
---
+++ src/test/java/com/i3create/cware/CWareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.i3create.cware;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CWareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
